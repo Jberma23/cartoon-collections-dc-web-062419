58603a410e62a4a46a1c3ae363f175a8eb0fea1d
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-17 15:40:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,10 +8,14 @@ def roll_call_dwarves(array)
   end
 end
 
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
 def summon_captain_planet(array)
   array.map! {|new_array| new_array.capitalize}
   new_array.each {|new_array| puts new_array + "!"}
 end
+summon_captain_planet(planeteer_calls)
+
 
 def long_planeteer_calls# code an argument here
   # Your code here
